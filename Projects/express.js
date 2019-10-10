aa9e17af38d46b420ca92b6f337a36d59ac494da
Alex Somoza <asomoza041@west-mec.org>
2019-10-09 18:22:44 -0700

    small changes
    changing page names

--- Projects/express.js
+++ Projects/express.js
@@ -5,27 +5,14 @@ const app = express();
 const port = 8080;
 
 var people = {
-    
-      "id": "ba924631-068e-4436-b6de-f3283fa848f0",
-      "name": "Ashitaka",
-      "gender": "male",
-      "age": "late teens",
-      "eye_color": "brown",
-      "hair_color": "brown",
-      "films": [
-        "https://ghibliapi.herokuapp.com/films/030555b3-4c92-4fce-93fb-e70c3ae3df8b"
-      ],
-      "species": "https://ghibliapi.herokuapp.com/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2",
-      "url": "https://ghibliapi.herokuapp.com/people/ba924631-068e-4436-b6de-f3283fa848f0"
-    },
-    {
-      "id": "030555b3-4c92-4fce-93fb-e70c3ae3df8b",
-      "name": "Yakul",
-      "age": "Unknown",
-      "gender": "male",
-      "eye_color": "Grey",
-      "hair_color": "Brown"
+      id: "ba924631-068e-4436-b6de-f3283fa848f0",
+      name: "Ashitaka",
+      gender: "male",
+      age: "late teens",
+      eye_color: "brown",
+      hair_color: "brown"
     }
+    
 
 
 app.set("view engine", "pug");
